618acb26e1f64c13c3e242740e32f1aade128887
kibirige <kibirigejohn005@gmail.com>
2023-01-09 16:50:02 +0300

    configure devise and letter opener

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
+     protect_from_forgery with: :exception
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+  # Catch all CanCan errors and alert the user of the exception
+#   rescue_from CanCan::AccessDenied do |exception|
+#     redirect_to inventories_url, alert: exception.message
+#   end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,8 @@ Rails.application.configure do
   # configuring devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # letter opener
+  config.action_mailer.delivery_method = :letter_opener
+config.action_mailer.perform_deliveries = true
+
 end
