e3af345864e384d2a41731d24190df4829361624
kibirige <kibirigejohn005@gmail.com>
2023-01-09 16:08:42 +0300

    add author to categories and transaction tables

--- app/models/ctegory.rb
+++ app/models/ctegory.rb
@@ -1,2 +1,7 @@
 class Ctegory < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+
+    has_many :transaction_categories
+    has_many :trnsactions, through: :transaction_categories
+
 end
--- app/models/trnsaction.rb
+++ app/models/trnsaction.rb
@@ -1,2 +1,8 @@
 class Trnsaction < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+
+    has_many :transaction_categories
+    has_many :ctegories, through: :transaction_categories
+
+    
 end
