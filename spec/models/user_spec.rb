c56fe0fdd8e49faf439f384f7b8cf941184f7d21
kibirige <kibirigejohn005@gmail.com>
2023-01-13 17:09:54 +0300

    add tests for the category model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject {
-    described_class.new(id: 1, name: 'john', email: 'john@john.com', created_at: DateTime.now, updated_at: DateTime.now)
+    described_class.new(name: 'john', email: 'jo@john.com',  password: '12345678', password_confirmation: '12345678')
   }
 
   describe "User " do
     it "is not valid with invalid attributes" do
-      expect(User.new).not_to be_valid
+      expect(User.new).to_not be_valid
     end
 
     it "is valid with valid attributes" do
-      expect(subject).not_to be_valid
+      subject.save
+      expect(subject).to be_valid
+    end
+
+    it "is valid when name is edited" do
+      subject.name = 'joh'
+      expect(subject).to be_valid
     end
   end
 
